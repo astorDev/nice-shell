2ffb6939299e85b0b0d7fe11de942a3d45645138
Egor Tarasov <pavluchenk0@yandex.ru>
2025-01-07 18:12:14 +0400

    installer

--- README.md
+++ README.md
@@ -3,6 +3,5 @@
 `log`, `throw`, and `ret` functions. Use:
 
 ```sh
-curl -s https://raw.githubusercontent.com/astorDev/nice-shell/main/.sh -o /tmp/nice-shell.sh
-source /tmp/nice-shell.sh
+source /dev/stdin <<< "$(curl -sS https://raw.githubusercontent.com/astorDev/nice-shell/refs/heads/main/.sh)"
 ```
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPO_URL="https://raw.githubusercontent.com/astorDev/nice-shell/refs/heads/installer/.sh"
+REPO_URL="https://raw.githubusercontent.com/astorDev/nice-shell/refs/heads/main/.sh"
 INSTALL_PATH="/usr/local/bin/nice-shell.sh"
 USER_PROFILE="$HOME/.bash_profile"  # Use .bash_profile for compatibility with macOS and Linux
 
@@ -12,11 +12,12 @@ chmod +x "$INSTALL_PATH"
 
 if ! grep -q "source $INSTALL_PATH" "$USER_PROFILE"; then
     echo "Adding the source line to $USER_PROFILE"
-    echo "\nsource $INSTALL_PATH" >> "$USER_PROFILE"
+    echo "\n\nsource $INSTALL_PATH" >> "$USER_PROFILE"
 fi
 
 echo "Installation complete! The profile will be automatically reloaded."
 
-exec bash
+source "$USER_PROFILE"
 
-log "We are ready to go! (See, the message is already nicer)"
+log "We are ready to go! (See, the message is already nice)"
+warn "You may need to restart the shell to use the functions."
