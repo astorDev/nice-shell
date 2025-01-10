a6897cc8048846efef734cce9fca3c87b5f5a473
Egor Tarasov <pavluchenk0@yandex.ru>
2025-01-10 14:35:19 +0400

    version-1-0

---
+++ versions/1.0/.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+LIGHT_CYAN='\033[1;36m'
+RED='\033[31m'
+NC='\033[0m'
+YELLOW='\033[1;33m'
+
+log() {
+    printf "${LIGHT_CYAN}${1}${NC}\n" >&2
+}
+
+throw() {
+    printf "${RED}${1}. Exiting...${NC}\n" >&2 
+    exit 1
+}
+
+warn() {
+    printf "${YELLOW}⚠️ ${1}${NC}\n" >&2 
+}
+
+ret() {
+    printf %s "${1}"
+    printf "\n" >&2
+}
---
+++ versions/1.0/index.md
@@ -0,0 +1,8 @@
+---
+date: 2024-01-10
+---
+
+# Nice Shell 1.0
+
+- [x] Using `printf` instead of `echo`
+- [x] `warn` command
