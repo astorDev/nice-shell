10b872e86b8fac53b9ce256841251b0487c8d059
Egor Tarasov <pavluchenk0@yandex.ru>
2025-03-03 16:24:47 +0300

    pushing

--- git/git-save.sh
+++ git/git-save.sh
@@ -1,15 +1,7 @@
 source /dev/stdin <<< "$(curl -sS https://raw.githubusercontent.com/astorDev/nice-shell/refs/heads/main/.sh)"
 
-COMMAND="git add -A"
-log "Executing '$COMMAND' (adding current changes to git)"
-$COMMAND
-
-COMMAND="git commit -m \"$1\""
-log "Executing '$COMMAND' (commiting changes to the current branch)"
+git add -A
 git commit -m "$1"
-
-
-# git add -A && 
-# git commit -m $1 && 
-# CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD) &&
-# echo "Current branch: $CURRENT_BRANCH"
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Current branch: $CURRENT_BRANCH"
+git push  --set-upstream origin $CURRENT_BRANCH
