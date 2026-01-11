13efcedd6ae52f47a180d5b7584fdf41b5f40780
Egor Tarasov <vosarat1995+1@gmail.com>
2026-01-11 15:59:17 +0100

    C# Initial Helper (#17)

---
+++ csharp/lib/AtomicCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace NiceShell;
+
+public class AtomicCommand
+{
+    public static string Run(string commmand)
+    {
+        var parts = commmand.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries);
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = parts[0],
+            Arguments = parts.Length > 1 ? parts[1] : string.Empty,
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        };
+
+        using var process = Process.Start(startInfo) ?? throw new InvalidOperationException("Failed to start process.");
+        var output = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+        return output;
+    }
+}
