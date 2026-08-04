4ca2b348501620ef0a707ac2cdd266033cd7be8c
Egor Tarasov <vosarat1995@google.com>
2026-08-04 07:58:50 +0200

    cli: `AddAsRootCommand`

--- cli/play/Program.cs
+++ cli/play/Program.cs
@@ -7,7 +7,13 @@ builder.Logging.AddNiceShell();
 
 builder.AddCommand<ExampleCommand>();
 builder.AddCommand<AlternativeCommand>("alt");
-//builder.AddCommand<AlternativeCommand>();
+
+// To check usage of the AlternativeCommand with it's original name:
+// builder.AddCommand<AlternativeCommand>();
+
+// Commands Upgraded to root scenario:
+// builder.AddAsRootCommand<ExampleCommand>("This is an example command that serves as the root command.");
+// builder.AddAsRootCommand<AlternativeCommand>("This is an alternative command that serves as the root command.");
 
 using var app = builder.Build("An example CLI application.");
 
