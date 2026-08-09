a47203adc177c4cd7aec94791080cfd2024b6d40
Egor Tarasov <vosarat1995@google.com>
2026-08-09 06:55:38 +0200

    play-template: Implemented & CI

---
+++ cli/play-template/Program.cs
@@ -0,0 +1,9 @@
+var builder = new CliBuilder();
+
+builder.Logging.AddNiceShell();
+
+builder.AddCommand<TemplateCommand>();
+
+using var app = builder.Build("A template CLI application.");
+
+return app.Run(args);
