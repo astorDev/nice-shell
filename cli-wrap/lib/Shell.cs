f94703a8f56b426978bebb05fac2c66ea8824db6
Egor Tarasov <vosarat1995@google.com>
2026-08-20 06:39:57 +0200

    cliwrap: Shell Update

--- cli-wrap/lib/Shell.cs
+++ cli-wrap/lib/Shell.cs
@@ -2,17 +2,34 @@
 
 namespace NiceShell;
 
-public record Shell(string File, string Flag)
+public record Shell(string File, params string[] Flags )
 {
-    public static readonly Shell Bash = new("/bin/bash", "-c");
-    public static readonly Shell Zsh = new("/bin/zsh", "-c");
-    public static readonly Shell Cmd = new("cmd.exe", "/c");
-    public static readonly Shell PowerShell = new("powershell.exe", "-Command");
-    public static readonly Shell Sh = new("/bin/sh", "-c");
+    public static readonly Shell Bash = new("bash", "-c");
+    public static readonly Shell Zsh = new("zsh", "-c");
+    public static readonly Shell Cmd = new("cmd", "/c");
+    public static readonly Shell PowerShell = new("powershell", "-Command");
+    public static readonly Shell Pwsh = new("pwsh", "-Command");
+    public static readonly Shell Sh = new("sh", "-c");
 
     public Command Proxy(string rawCommand)
     {
         return Cli.Wrap(File)
-            .WithArguments([Flag, rawCommand]);
+            .WithArguments([ ..Flags, rawCommand]);
+    }
+
+    public override string ToString()
+    {
+        return String.Join(";", [ File, ..Flags ]);
+    }
+
+    public static Shell Parse(string shellString)
+    {
+        var parts = shellString.Split(";", StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length == 0)
+            throw new ArgumentException("Shell string cannot be empty.", nameof(shellString));
+
+        var file = parts[0];
+        var flags = parts.Skip(1).ToArray();
+        return new Shell(file, flags);
     }
 }
--- cli-wrap/play/RunCommand.cs
+++ cli-wrap/play/RunCommand.cs
@@ -4,44 +4,40 @@ namespace Playground;
 
 public class RunCommand : Command
 {
-    private readonly Option<string> nameOption = new("--name")
+    private readonly Argument<string> commandKeyArgument = new("command")
     {
-        Description = "The name of the person to greet.",
-        Required = true
+        Description = "The command to execute.",
+        Arity = ArgumentArity.ExactlyOne
     };
 
-    private readonly Argument<string> pathArgument = new("path")
+    private readonly Dictionary<string, string> commandKeyMap = new()
     {
-        Description = "The path from which to read files.",
-        Arity = ArgumentArity.ExactlyOne
+        { "echo", @"echo ""Hello, World!"" || true && echo ""Second line""" },
+        { "replace", @"replace --all-cases SomethingElse SomethingElse" }
     };
     
     private readonly ILogger<RunCommand> logger;
 
     public RunCommand(ILogger<RunCommand> logger) : base("run", "Greet a person by name.")
     {
-        Add(pathArgument);
-        Add(nameOption);
+        Add(commandKeyArgument);
         SetAction(Execute);
 
         this.logger = logger;
     }
 
-    private void Execute(ParseResult parseResult)
+    private async Task Execute(ParseResult parseResult)
     {
-        var name = parseResult.GetRequiredValue(nameOption);
-        var path = parseResult.GetRequiredValue(pathArgument);
+        var key = parseResult.GetValue(commandKeyArgument) ?? "echo";
+        if (!commandKeyMap.TryGetValue(key, out var command))
+        {
+            logger.LogWarning("Unknown command key: {Command}. Defaulting to 'echo'.", key);
+            command = commandKeyMap["echo"];
+        }
 
-        logger.LogDebug("Greeting {Name}...", name);
-        Console.WriteLine($"Hello, {name}!");
-        logger.LogInformation("Greeted {Name} successfully.", name);
-
-        logger.LogDebug("Getting entries in the path: `{Path}`", path);
-        var entryNames = Directory.GetFileSystemEntries(path)
-            .Select(Path.GetFileName)
-            .ToList();
-        logger.LogInformation("Found {Count} entries in the path: `{Path}`. Writing to the output...", entryNames.Count, path);
-
-        entryNames.ForEach(Console.WriteLine);
+        await Shell.Sh
+            .Proxy(command)
+            .WithConsoleForwarding()
+            .ExecuteAsync();
     }
 }
