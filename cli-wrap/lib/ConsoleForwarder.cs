24d0b6ce3ed419d9cab8bdd1e4f2a9f7256dceb8
Egor Tarasov <vosarat1995+1@gmail.com>
2026-08-20 07:09:14 +0200

    Cliwrap shell update + dimmed console forwarding support (#25)
    * cliwrap: Shell Update
    
    * Dimmed Console Forwarding
    
    ---------
    
    Co-authored-by: Egor Tarasov <vosarat1995@google.com>

--- cli-wrap/lib/ConsoleForwarder.cs
+++ cli-wrap/lib/ConsoleForwarder.cs
@@ -9,4 +9,20 @@ public static class ConsoleForwarder
         return command.WithStandardOutputPipe(PipeTarget.ToStream(Console.OpenStandardOutput()))
             .WithStandardErrorPipe(PipeTarget.ToStream(Console.OpenStandardError()));
     }
+
+    public static Command WithDimmedConsoleForwarding(this Command command)
+    {
+        return command.WithStandardOutputPipe(ConsoleForwarding.Dimmed(Console.Out))
+            .WithStandardErrorPipe(ConsoleForwarding.Dimmed(Console.Error));
+    }
+}
+
+public class ConsoleForwarding
+{
+    public static PipeTarget Dimmed(TextWriter writer) => PipeTarget.ToDelegate(line =>
+    {
+        writer.Write("\x1b[2m");
+        writer.WriteLine(line);
+        writer.Write("\x1b[22m");
+    });
 }
--- cli-wrap/play/Program.cs
+++ cli-wrap/play/Program.cs
@@ -6,11 +6,4 @@ builder.AddCommand<RunCommand>();
 
 using var app = builder.Build("A niceshell.cliwrap CLI application.");
 
-var rawCommand = @"echo ""Hello, World!"" || true && echo ""Second line""";
-
-await Shell.Bash
-    .Proxy(rawCommand)
-    .WithConsoleForwarding()
-    .ExecuteAsync();
-
-// return app.Run(args);
+return app.Run(args);
